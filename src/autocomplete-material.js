5f2466b83fa1e768f2b4e4d1aa4889ab4c4003d8
Slawek Kaczorowski <skaczorowski@opera.com>
2016-01-14 12:16:06 +0100

    Added refresh event

--- src/autocomplete-material.js
+++ src/autocomplete-material.js
@@ -1,13 +1,18 @@
 angular.module('autocompleteMaterial', [
-  'schemaForm',
-  'templates'
-]).config(function(schemaFormDecoratorsProvider, sfBuilderProvider) {
+    'schemaForm',
+    'templates'
+]).config(function (schemaFormDecoratorsProvider, sfBuilderProvider) {
+    schemaFormDecoratorsProvider.defineAddOn(
+        'bootstrapDecorator',           // Name of the decorator you want to add to.
+        'autocomplete-material',                      // Form type that should render this add-on
+        'src/templates/autocomplete-material.html',  // Template name in $templateCache
+        sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+        );
 
-  schemaFormDecoratorsProvider.defineAddOn(
-    'bootstrapDecorator',           // Name of the decorator you want to add to.
-    'autocomplete-material',                      // Form type that should render this add-on
-    'src/templates/autocomplete-material.html',  // Template name in $templateCache
-    sfBuilderProvider.stdBuilders   // List of builder functions to apply.
-  );
-
+}).controller('autocompleterCtrl', function ($scope) {
+    $scope.$on('refreshAutocompleter', function (e, key) {
+        if (_.isEqual($scope.$parent.form.key, key)) {
+            $scope.$$childHead.$mdAutocompleteCtrl.clear();  
+        }
+    })
 });
